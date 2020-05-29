998147cfc717b03c6967c0cef798b7f5de0ba244
favonia <favonia@gmail.com>
2020-05-29 05:58:12 -0500

    Errors when replacement is not used (e.g., !(x -> y)).

--- src/Action.ml
+++ src/Action.ml
@@ -5,12 +5,16 @@ let flip_inversion =
   | `Normal -> `Negated
   | `Negated -> `Normal
 
+type path = string list
+
+type error = ReplacementNotUsed of path * path
+
 type exportability = [`Public | `Private]
 
 type action =
   | ActWildcard
-  | ActId of string list * string list
-  | ActScope of string list * string list * action
+  | ActId of path * path option
+  | ActScope of path * path option * action
   | ActSeq of action * action (* will be n-ary later *)
   | ActNeg of action
   | ActExport of exportability * action
@@ -26,56 +30,65 @@ let rec trim_prefix prefix path =
 
 let rec run ~inversion ~export action path =
   match inversion, action, path with
-  | `Normal, ActWildcard, [] -> NoMatch
-  | `Negated, ActWildcard, [] -> Matched (export, [])
-  | `Normal, ActWildcard, (_ :: _) -> Matched (export, path)
-  | `Negated, ActWildcard, (_ :: _) -> NoMatch
-  | _, ActScope (prefix, replacement, action), path ->
+  | `Normal, ActWildcard, [] -> Ok NoMatch
+  | `Negated, ActWildcard, [] -> Ok (Matched (export, []))
+  | `Normal, ActWildcard, (_ :: _) -> Ok (Matched (export, path))
+  | `Negated, ActWildcard, (_ :: _) -> Ok NoMatch
+  | `Negated, ActScope (prefix, Some prefix_replacement, _), _ ->
+    Error (ReplacementNotUsed (prefix, prefix_replacement))
+  | _, ActScope (prefix, prefix_replacement, action), path ->
     begin
       match trim_prefix prefix path with
       | None ->
         begin
           match inversion with
-          | `Normal -> NoMatch
-          | `Negated -> Matched (export, path)
+          | `Normal -> Ok NoMatch
+          | `Negated -> Ok (Matched (export, path))
         end
       | Some remaining ->
-        begin
-          match run ~inversion ~export action remaining with
+        let prefix_replacement = Option.value prefix_replacement ~default:prefix in
+        run ~inversion ~export action remaining |> Result.map begin
+          function
           | NoMatch -> NoMatch
-          | Matched (export, remaining_replacement) -> Matched (export, replacement @ remaining_replacement)
+          | Matched (export, remaining_replacement) -> Matched (export, prefix_replacement @ remaining_replacement)
         end
     end
-  | `Normal, ActId (prefix, replacement), path ->
+  | `Normal, ActId (prefix, prefix_replacement), path ->
+    let prefix_replacement = Option.value prefix_replacement ~default:prefix in
     begin
       match trim_prefix prefix path with
-      | None -> NoMatch
-      | Some remaining -> Matched (export, replacement @ remaining)
+      | None -> Ok NoMatch
+      | Some remaining -> Ok (Matched (export, prefix_replacement @ remaining))
     end
-  | `Negated, ActId (prefix, _), path ->
-    (* TODO should warn user if replacement != prefix *)
+  | `Negated, ActId (prefix, Some prefix_replacement), _ ->
+    Error (ReplacementNotUsed (prefix, prefix_replacement))
+  | `Negated, ActId (prefix, None), path ->
     begin
       match trim_prefix prefix path with
-      | None -> Matched (export, path)
-      | Some _ -> NoMatch
+      | None -> Ok (Matched (export, path))
+      | Some _ -> Ok NoMatch
     end
   | `Normal, ActSeq (act1, act2), path ->
     begin
-      match run ~inversion ~export act1 path with
+      Result.bind (run ~inversion ~export act1 path) @@
+      function
       | NoMatch -> run ~inversion ~export act2 path
       | Matched (export, replacement1) ->
-        match run ~inversion ~export act2 replacement1 with
-        | NoMatch -> Matched (export, replacement1)
-        | Matched (export, replacement2) -> Matched (export, replacement2)
+        Result.bind (run ~inversion ~export act2 replacement1) @@
+        function
+        | NoMatch -> Ok (Matched (export, replacement1))
+        | Matched (export, replacement2) -> Ok (Matched (export, replacement2))
     end
   | `Negated, ActSeq (act1, act2), path ->
     begin
-      match run ~inversion ~export act1 path with
-      | NoMatch -> NoMatch
+      Result.bind (run ~inversion ~export act1 path) @@
+      function
+      | NoMatch -> Ok NoMatch
       | Matched (export, replacement1) ->
-        match run ~inversion ~export act2 replacement1 with
-        | NoMatch -> NoMatch
-        | Matched (export, replacement2) -> Matched (export, replacement2)
+        Result.bind (run ~inversion ~export act2 replacement1) @@
+        function
+        | NoMatch -> Ok NoMatch
+        | Matched (export, replacement2) -> Ok (Matched (export, replacement2))
     end
   | _, ActNeg act, path -> run ~inversion:(flip_inversion inversion) ~export act path
   | _, ActExport (export, act), path -> run ~inversion ~export act path
