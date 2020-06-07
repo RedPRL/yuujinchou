64493ea6a1e31db5db1b08be5932d8efb9b8034a
favonia <favonia@gmail.com>
2020-06-06 21:45:49 -0500

    Add Action.run_ for [unit pattern].

--- src/Action.mli
+++ src/Action.mli
@@ -6,8 +6,10 @@ type 'a error =
 
 type 'a result_ = [ `NoMatch | `Matched of (path * 'a) list ]
 
-val check : 'a pattern -> (unit, 'a error) result
 val run : default:'a -> join:('a->'a->'a) -> meet:('a->'a->'a) -> 'a pattern -> path -> ('a result_, 'a error) result
+val run_ : unit pattern -> path -> (unit result_, unit error) result
+
+val check : 'a pattern -> (unit, 'a error) result
 
 val pp_result_ : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a result_ -> unit
 val pp_result : (Format.formatter -> 'a -> unit) -> Format.formatter -> ('a result_, 'a error) result -> unit
