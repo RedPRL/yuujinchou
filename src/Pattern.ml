2f8e7a978ee1a00138c7f58a6e44da7c22539dad
favonia <favonia@gmail.com>
2020-07-17 19:55:07 -0500

    Revert "Follow ocamlformat."
    Will ocaml-ci be happy about this?

--- src/Pattern.ml
+++ src/Pattern.ml
@@ -1,4 +1,5 @@
-type path = string list [@@deriving show]
+type path = string list
+[@@deriving show]
 
 type 'a pattern =
   | PatWildcard
@@ -9,53 +10,44 @@ type 'a pattern =
   | PatAttr of 'a * 'a pattern
 [@@deriving show]
 
-let inv = function PatInv p -> p | p -> PatInv p
-
+let inv =
+  function
+  | PatInv p -> p
+  | p -> PatInv p
 let wildcard = PatWildcard
-
 let root = inv wildcard
-
-let rec scope = function
+let rec scope =
+  function
   | [] -> fun p -> p
-  | s -> (
-      function
-      | PatScope (s2, None, p) -> scope (s @ s2) p | p -> PatScope (s, None, p)
-      )
-
+  | s ->
+    function
+    | PatScope (s2, None, p) -> scope (s @ s2) p
+    | p -> PatScope (s, None, p)
 let rec renaming_scope s s' =
-  if s = [] && s' = [] then fun p -> p
-  else function
+  if s = [] && s' = [] then
+    fun p -> p
+  else
+    function
     | PatScope (s2, Some s'2, p) -> renaming_scope (s @ s2) (s' @ s'2) p
     | p -> PatScope (s, Some s', p)
-
 let seq acts = PatSeq acts
-
 let seq_filter acts = inv @@ seq @@ List.map inv acts
-
 let none = seq []
-
 let any = inv none
-
 let only x = scope x root
-
 let renaming x x' = renaming_scope x x' root
-
 let prefix x = scope x any
-
 let renaming_prefix x x' = renaming_scope x x' any
-
-let rec attr a = function PatAttr (a, p) -> attr a p | p -> PatAttr (a, p)
-
+let rec attr a =
+  function
+  | PatAttr (a, p) -> attr a p
+  | p -> PatAttr (a, p)
 let join l = PatJoin l
-
 let unsafe_meet l = inv @@ join @@ List.map inv l
-
-let meet = function
+let meet =
+  function
   | [] -> invalid_arg "Pattern.meet: empty list"
   | l -> unsafe_meet l
-
 let except x = inv @@ only x
-
 let except_prefix x = inv @@ prefix x
-
 let unsafe_inv = inv
