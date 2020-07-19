f91d21f24e4ba0382f9b4583fa896901ef41edb0
favonia <favonia@gmail.com>
2020-07-19 18:56:22 -0500

    New start.

--- src-new/Trie.ml
+++ src-new/Trie.ml
@@ -1,5 +1,7 @@
 open StdLabels
 
+type path = string list
+
 module StringMap = Map.Make (struct
     type t = string
     let compare = String.compare
@@ -10,8 +12,6 @@ type 'a node = {
   children : 'a node StringMap.t;
 }
 
-type path = string list
-
 type 'a t = 'a node option
 
 let empty : 'a t = None
@@ -20,17 +20,16 @@ let is_empty : 'a t -> bool = Option.is_none
 
 let non_empty (t : 'a node) : 'a t = Some t
 
-(** could be empty *)
+(** {1 Making (non-empty) trees} *)
+
 let mk_tree root children =
   if Option.is_none root && StringMap.is_empty children
   then empty
   else non_empty {root; children}
 
-let root_node data = {root = Some data; children = StringMap.empty}
+let mk_root_node data = {root = Some data; children = StringMap.empty}
 
-let root_opt root = Option.map root_node root
-
-let root data = non_empty @@ root_node data
+let mk_root_opt root = Option.map mk_root_node root
 
 let rec prefix_node path t : 'a node =
   let f seg t =
@@ -40,24 +39,43 @@ let rec prefix_node path t : 'a node =
 
 let prefix path = Option.map @@ prefix_node path
 
-let singleton_node (path, data) = prefix_node path @@ root_node data
+let singleton_node (path, data) = prefix_node path @@ mk_root_node data
 
 let singleton (path, data) = non_empty @@ singleton_node (path, data)
 
-let rec find_opt_node_cont path t k =
+let root data = non_empty @@ mk_root_node data
+
+(** {1 Getting data} *)
+
+let rec find_node_cont path t k =
   match path with
   | [] -> k t
   | seg::path ->
     Option.bind (StringMap.find_opt seg t.children) @@ fun t ->
-    find_opt_node_cont path t k
+    find_node_cont path t k
 
-let find_opt path t =
-  Option.bind t @@ fun t -> find_opt_node_cont path t @@ fun t -> t.root
+let find_prefix path t =
+  Option.bind t @@ fun t -> find_node_cont path t non_empty
 
-let find_root_opt t = find_opt [] t
+let find_singleton path t =
+  Option.bind t @@ fun t -> find_node_cont path t @@ fun t -> t.root
 
-let find_subtree path t =
-  Option.bind t @@ fun t -> find_opt_node_cont path t non_empty
+let find_root t = find_singleton [] t
+
+(** {1 Traversing the trees} *)
+
+let rec update_node_cont t path k =
+  match path with
+  | [] -> k @@ non_empty t
+  | seg::path ->
+    mk_tree t.root @@ StringMap.update seg (fun t -> update_cont t path k) t.children
+
+and update_cont t path k =
+  match t with
+  | None -> prefix path @@ k empty
+  | Some t -> update_node_cont t path k
+
+(** {1 Union} *)
 
 let union_option f x x' =
   match x, x' with
@@ -75,17 +93,6 @@ let rec union_node m t t' =
 
 let union m = union_option @@ union_node m
 
-let rec update_node_cont t path k =
-  match path with
-  | [] -> k @@ non_empty t
-  | seg::path ->
-    mk_tree t.root @@ StringMap.update seg (fun t -> update_cont t path k) t.children
-
-and update_cont t path k =
-  match t with
-  | None -> prefix path @@ k empty
-  | Some t -> update_node_cont t path k
-
 let union_subtree m t (path, t') =
   match t, t' with
   | None, _ -> prefix path t'
@@ -99,22 +106,23 @@ let union_singleton m t (path, data) =
   match t with
   | None -> singleton (path, data)
   | Some t -> update_node_cont t path @@ function
-    | None -> root data
+    | None -> non_empty @@ mk_root_node data
     | Some t -> non_empty {t with root = union_option m t.root @@ Some data}
 
-let intersect_option f x x' =
-  match x, x' with
-  | _, None | None, _ -> None
-  | Some x, Some x' -> f x x'
+let union_root m t data = union_singleton m t ([], data)
 
-let rec intersect_node m t t' =
-  let root = intersect_option (fun t t' -> Some (m t t')) t.root t'.root in
-  let children =
-    StringMap.merge (fun _key -> intersect m) t'.children t.children
-  in
-  mk_tree root children
+(** {1 Updating trees} *)
 
-and intersect m = intersect_option @@ intersect_node m
+let update_subtree path f t = update_cont t path f
+
+let update_singleton path f t = update_cont t path @@
+  function
+  | None -> mk_root_opt @@ f None
+  | Some t -> mk_tree (f t.root) t.children
+
+let update_root f t = update_singleton [] f t
+
+(** {1 Detaching subtrees} *)
 
 let rec update_extra_node_cont path t k =
   match path with
@@ -137,14 +145,7 @@ let detach_singleton path t = update_extra_cont path t @@ function
 
 let detach_root t = detach_singleton [] t
 
-let update_subtree path f t = update_cont t path f
-
-let update_singleton path f t = update_cont t path @@
-  function
-  | None -> root_opt @@ f None
-  | Some t -> mk_tree (f t.root) t.children
-
-let update_root f t = update_singleton [] f t
+(** {1 Conversion from/to Seq} *)
 
 let rec node_to_seq prefix_stack t () =
   match t.root with
--- src-new/Trie.mli
+++ src-new/Trie.mli
@@ -6,40 +6,26 @@ val empty : 'a t
 
 val is_empty : 'a t -> bool
 
+val prefix : path -> 'a t -> 'a t
+val singleton : path * 'a -> 'a t
 val root : 'a -> 'a t
 
-val root_opt : 'a option -> 'a t
-
-val prefix : path -> 'a t -> 'a t
-
-val singleton : path * 'a -> 'a t
-
-val find_opt : path -> 'a t -> 'a option
-
-val find_subtree : path -> 'a t -> 'a t
-
-val find_root_opt : 'a t -> 'a option
+val find_prefix : path -> 'a t -> 'a t
+val find_singleton : path -> 'a t -> 'a option
+val find_root : 'a t -> 'a option
 
 val union : ('a -> 'a -> 'a) -> 'a t -> 'a t -> 'a t
-
 val union_subtree : ('a -> 'a -> 'a) -> 'a t -> path * 'a t -> 'a t
-
 val union_singleton : ('a -> 'a -> 'a) -> 'a t -> path * 'a -> 'a t
-
-val intersect : ('a -> 'a -> 'a) -> 'a t -> 'a t -> 'a t
-
-val detach_subtree : path -> 'a t -> 'a t * 'a t
-
-val detach_singleton : path -> 'a t -> 'a t * 'a option
-
-val detach_root : 'a t -> 'a t * 'a option
+val union_root : ('a -> 'a -> 'a) -> 'a t -> 'a -> 'a t
 
 val update_subtree : path -> ('a t -> 'a t) -> 'a t -> 'a t
-
 val update_singleton : path -> ('a option -> 'a option) -> 'a t -> 'a t
-
 val update_root : ('a option -> 'a option) -> 'a t -> 'a t
 
-val to_seq : 'a t -> (path * 'a) Seq.t
+val detach_subtree : path -> 'a t -> 'a t * 'a t
+val detach_singleton : path -> 'a t -> 'a t * 'a option
+val detach_root : 'a t -> 'a t * 'a option
 
+val to_seq : 'a t -> (path * 'a) Seq.t
 val of_seq : ('a -> 'a -> 'a) -> (path * 'a) Seq.t -> 'a t
