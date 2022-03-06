9885532df23faf985dacb661ba1efed444ede013
favonia <favonia@gmail.com>
2022-03-06 08:55:31 -0600

    feat(action): monadic union (#57)
    * feat(trie): monadic union
    
    * feat(action): monadic engine with Stdlib.result
    
    BREAKING CHANGES: run and run_with_hooks now take a monadic union
    
    * fix(sequtils): make OCaml 4.08 happy

--- src/Action.ml
+++ src/Action.ml
@@ -5,7 +5,7 @@ open ResultMonad.Syntax
 type path = Pattern.path
 let pp_path = Pattern.pp_path
 
-type nonrec ('a, 'error) result = ('a Trie.t, [> `BindingNotFound of path] as 'error) result
+type nonrec ('a, 'error) result = ('a, [> `BindingNotFound of path] as 'error) result
 
 let check_nonempty ~path t =
   if Trie.is_empty t then
@@ -27,14 +27,17 @@ let rec run_ ~union ~hooks ~rev_prefix pat t =
     Trie.Result.update_subtree p (run_ ~union ~hooks ~rev_prefix:(List.rev_append p rev_prefix) pat) t
   | P_renaming (p1, p2) ->
     let t, remaining = Trie.detach_subtree p1 t in
-    let+ () = check_nonempty ~path:(List.rev_append rev_prefix p1) t in
-    Trie.union_subtree ~rev_prefix union remaining (p2, t)
+    let* () = check_nonempty ~path:(List.rev_append rev_prefix p1) t in
+    Trie.Result.union_subtree ~rev_prefix union remaining (p2, t)
   | P_seq pats ->
     let f t pat = t >>= run_ ~union ~hooks ~rev_prefix pat in
     List.fold_left ~f ~init:(ret t) pats
   | P_union pats ->
-    let+ ts = ResultMonad.map (fun pat -> run_ ~union ~hooks ~rev_prefix pat t) pats in
-    List.fold_left ~f:(Trie.union union ~rev_prefix) ~init:Trie.empty ts
+    let f ts pat =
+      let* ti = run_ ~union ~hooks ~rev_prefix pat t in
+      Trie.Result.union ~rev_prefix union ts ti
+    in
+    ResultMonad.fold_left ~f ~init:Trie.empty pats
   | P_hook h -> hooks h ~rev_prefix t
 
 let run_with_hooks ?(rev_prefix=[]) ~union ~hooks = run_ ~union ~hooks ~rev_prefix
