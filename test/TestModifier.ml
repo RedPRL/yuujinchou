163648f811f0f8a63716e851db9854565e953f11
favonia <favonia@gmail.com>
2023-09-29 06:59:31 -0500

    style: rename exception names

--- test/TestModifier.ml
+++ test/TestModifier.ml
@@ -32,10 +32,10 @@ let data : data Alcotest.testable =
 type empty = |
 module M = Modifier.Make (struct type nonrec data = data type tag = unit type hook = empty type context = empty end)
 
-exception WrappedBindingNotFound of Trie.bwd_path
+exception Wrapped_binding_not_found of Trie.bwd_path
 
 let wrap f =
-  let not_found _ prefix = raise @@ WrappedBindingNotFound prefix
+  let not_found _ prefix = raise @@ Wrapped_binding_not_found prefix
   and shadow _ path (x, ()) (y, ()) = U (path, x, y), ()
   and hook _ _ = function (_ : empty) -> .
   in
@@ -57,7 +57,7 @@ let test_none_2 () =
 
 let test_none_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound Emp)
+    (Wrapped_binding_not_found Emp)
     (wrap_error @@ fun () -> M.modify none Trie.empty)
 
 let test_all_1 () =
@@ -72,7 +72,7 @@ let test_all_2 () =
 
 let test_all_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound Emp)
+    (Wrapped_binding_not_found Emp)
     (wrap_error @@ fun () -> M.modify all Trie.empty)
 
 let test_all_4 () =
@@ -93,7 +93,7 @@ let test_only_2 () =
 
 let test_only_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound (Emp <: "x" <: "y"))
+    (Wrapped_binding_not_found (Emp <: "x" <: "y"))
     (wrap_error @@ fun () -> M.modify (only ["x"; "y"]) Trie.empty)
 
 let test_only_4 () =
@@ -114,7 +114,7 @@ let test_except_2 () =
 
 let test_except_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound (Emp <: "x" <: "y"))
+    (Wrapped_binding_not_found (Emp <: "x" <: "y"))
     (wrap_error @@ fun () -> M.modify (except ["x"; "y"]) Trie.empty)
 
 let test_in_1 () =
@@ -129,7 +129,7 @@ let test_in_2 () =
 
 let test_in_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound (Emp <: "x"))
+    (Wrapped_binding_not_found (Emp <: "x"))
     (wrap_error @@ fun () -> M.modify (in_ ["x"] all) Trie.empty)
 
 let test_in_4 () =
@@ -150,7 +150,7 @@ let test_renaming_2 () =
 
 let test_renaming_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound (Emp <: "x"))
+    (Wrapped_binding_not_found (Emp <: "x"))
     (wrap_error @@ fun () -> M.modify (renaming ["x"] ["z"]) Trie.empty)
 
 let test_renaming_4 () =
@@ -179,17 +179,17 @@ let test_seq_1 () =
 
 let test_seq_2 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound Emp)
+    (Wrapped_binding_not_found Emp)
     (wrap_error @@ fun () -> M.modify (seq [none; all]) (of_list [["x"; "y"], N 10; ["x"; "x"], N 20; ["y"], N 30]))
 
 let test_seq_3 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound Emp)
+    (Wrapped_binding_not_found Emp)
     (wrap_error @@ fun () -> M.modify (seq [none; none]) Trie.empty)
 
 let test_seq_4 () =
   Alcotest.check_raises "error"
-    (WrappedBindingNotFound (Emp <: "x"))
+    (Wrapped_binding_not_found (Emp <: "x"))
     (wrap_error @@ fun () -> M.modify (seq [renaming ["x"] ["z"]; only ["x"]]) (of_list [["x"], N 10; ["y"], N 20]))
 
 let test_seq_5 () =
